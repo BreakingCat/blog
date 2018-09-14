24e50d362a15befb3d0ee43b0d5a9348b723e0f3
Raphael <531642833@qq.com>
2018-09-15 00:52:58 +0800

    Add feature: comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,6 +10,8 @@ class ArticlesController < ApplicationController
   # GET /articles/1
   # GET /articles/1.json
   def show
+    @comment = @article.comments.new
+    @comments = @article.comments
   end
 
   # GET /articles/new
